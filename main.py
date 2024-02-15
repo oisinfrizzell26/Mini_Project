216a3b9626e6424507556f9ddb10630891503880
oisinfrizzell26 <oisinfrizzell26@gmail.com>
2024-02-15 13:20:50 +0000

    testing

--- main.py
+++ main.py
@@ -2,13 +2,13 @@ from flask import Flask, redirect, url_for, render_template, request
 
 app = Flask(__name__)
 
-@app.route("/home")
+
+
+@app.route("/")
 def home():
     return render_template("home.html")
 
-@app.route("/")
-def login():
-    return render_template("login.html")
+
 
 if __name__ == '__main__':
     app.run(debug=True)
